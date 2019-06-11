6bc6efd5b321a9bcfcfad28328e7b20075c98351
keijor <keijo.raamat@proekspert.ee>
2019-06-11 15:38:54 +0300

    removes GameRepository

--- src/main/java/ee/proekspert/Repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.proekspert.Repository;
-
-import ee.proekspert.Domain.GameEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GameRepository extends MongoRepository<GameEntity, String> {
-
-}
-
--- src/main/java/ee/proekspert/Service/Implementation/QuestionServiceImplementation.java
+++ src/main/java/ee/proekspert/Service/Implementation/QuestionServiceImplementation.java
@@ -4,23 +4,19 @@ import ee.proekspert.DTO.QuestionNextDTO;
 import ee.proekspert.DTO.QuestionSendDTO;
 import ee.proekspert.Domain.GameEntity;
 import ee.proekspert.Domain.QuestionEntity;
-import ee.proekspert.Repository.GameRepository;
 import ee.proekspert.Repository.QuestionRepository;
 import ee.proekspert.Service.QuestionService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class QuestionServiceImplementation implements QuestionService {
     private final QuestionRepository repository;
-    private final GameRepository gameRepository;
 
-    public QuestionServiceImplementation(QuestionRepository repository, GameRepository gameRepository) {
+    public QuestionServiceImplementation(QuestionRepository repository) {
         this.repository = repository;
-        this.gameRepository = gameRepository;
     }
 
     @Override
@@ -32,7 +28,6 @@ public class QuestionServiceImplementation implements QuestionService {
     @Override
     public QuestionSendDTO startNewGameAndSendFirstQuestion() {
         GameEntity newGame = new GameEntity();
-        gameRepository.save(newGame);
         QuestionEntity question = repository.findByQuestionNumber("1");
         QuestionSendDTO questionDTO = new QuestionSendDTO();
         BeanUtils.copyProperties(question, questionDTO);
